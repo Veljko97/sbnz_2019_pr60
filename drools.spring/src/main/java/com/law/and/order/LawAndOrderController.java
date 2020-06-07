04c3c12b3e57bc0576109b84cdb828335f979fce
Veljko Vucenic <vmvucenic@live.com>
2020-06-07 22:15:59 +0200

    serve rules file

--- drools.spring/src/main/java/com/law/and/order/LawAndOrderController.java
+++ drools.spring/src/main/java/com/law/and/order/LawAndOrderController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,12 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import com.law.and.order.facts.CrimeClasification;
 import com.law.and.order.facts.enums.ActionTypesDelaProtivImovine;
 import com.law.and.order.facts.enums.ActionTypes;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 @RestController
 public class LawAndOrderController {
@@ -35,6 +42,17 @@ public class LawAndOrderController {
 		CrimeClasification crimeClasification = lawAndOrderService.getCrime(actionTypes);
 		return ResponseEntity.ok(crimeClasification);
 	}
-	
-	
+
+	@RequestMapping(value = "/getRules", method = RequestMethod.GET)
+	public ResponseEntity<?> getRules(@RequestParam(required = true) String ruleSet) throws FileNotFoundException {
+		File file = ResourceUtils.getFile("classpath:drools/rules/" + ruleSet + ".drl");
+
+		BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+		String text = "";
+		Scanner scanner = new Scanner(file);
+		while (scanner.hasNext()){
+			text += scanner.nextLine() + '\n';
+		}
+		return  ResponseEntity.ok(text);
+	}
 }
